0f6d5ec40b146f496796466c29ebe98d2dba6d02
Mary Alex <68873062+Ligzandra@users.noreply.github.com>
2022-06-03 22:10:11 +0000

    cleaned code

--- index.js
+++ index.js
@@ -9,7 +9,5 @@ let userGuess=prompt("Guess a number between 1 and 2");
 
 if(userGuess == randomNum){
   alert(`${username} scores 2`)
- // let newRange= rangeNum.lastIndexOf()
- //  console.log(newRange)
 }
 else{alert(`${username} needs to try again`)}  
