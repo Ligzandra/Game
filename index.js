90a6a05866036dc9999a35dcd327c517dadbda14
Mary Alex <68873062+Ligzandra@users.noreply.github.com>
2022-06-03 22:45:40 +0000

    range function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let rangeNum=[1,2]
 let username;
 
-let randomNum=Math.floor(Math.random()*(rangeNumh-1)+1)
-console.log(randomNum)
+let randomNum;
+
+function getRandomNum(min, max) {
+  return randomNum= Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(getRandomNum(1,2))
 
 username=prompt("what is your username?")
 let userGuess=prompt("Guess a number between 1 and 2");
 
 if(userGuess == randomNum){
-  alert(`${username} scores 2`)
+   alert(`${username}, scores 2`)
+
 }
-else{alert(`${username} needs to try again`)}  
+else{alert(`${username} needs to try again`);  }  
