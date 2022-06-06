4859e75a5a4117c7dccb56ae3ecd2b06cf7baffa
Mary Alex <68873062+Ligzandra@users.noreply.github.com>
2022-06-06 09:42:39 +0000

    implemented the do and while loop

--- index.js
+++ index.js
@@ -13,30 +13,33 @@ function start(){
   username=prompt("what is your username?")
   startGame()
 }
+start()
 
 function startGame(){
-   console.log(`step: ${step}`)
+
+do{
+  console.log(`step: ${step}`)
   userRange=prompt(`${username}, Select a range from 1 to another number`)
-    getRandomNum(1, userRange)
-  userGuess=prompt(`Guess a number between 1 and ${userRange}`);     
-  userResult()
-}
-function userResult(){
+ getRandomNum(1, userRange)
+  userGuess=prompt(`Guess a number between 1 and ${userRange}`); 
+  
   if(userGuess == randomNum){   
     score+=2
    alert(`${username},scores ${score}`)    
     step+=1
-    while(step <= 2){    
-    startGame()
+    }
+  else {alert(`${username} needs to try again`);start();  } 
+}
+  
+while(step <= 2){     
     if(step >= 2){
      alert(`GAME OVER! \n ${username},scores a total of ${score} points`);
-   break;
     } 
-  }  
-  }    
-else{alert(`${username} needs to try again`);start();  } 
+}  
+      
+}
 
-} 
-start()
+
+
 
 
